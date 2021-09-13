7cd632ad30c3aa3af13a28ea4860a98f5f6c1b84
Ryan <tamielryan@gmail.com>
2021-09-13 12:17:25 +0900

    Fruits finished

--- fruits/fruits/App/OnboardingView.swift
+++ fruits/fruits/App/OnboardingView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - PROPERTIES
-    
+    var fruits: [Fruit] = fruitsData
     // MARK: - BODY
     var body: some View {
         TabView{
-            ForEach(0..<5) { item in
-                FruitCardView()
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
                 } //: LOOP
-            FruitCardView()
         }   //: TAB
         .tabViewStyle(PageTabViewStyle())
         .padding(.vertical, 20)
@@ -25,6 +24,6 @@ struct OnboardingView: View {
 // MARK: - PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
