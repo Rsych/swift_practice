fa4bb9b17b680ee91b1980966e5ab0140a34ceed
Ryan <tamielryan@gmail.com>
2021-10-03 12:46:02 +0900

    custom container

--- 100days/Day 23 ~ 24/Project 3/Project 3/ContentView.swift
+++ 100days/Day 23 ~ 24/Project 3/Project 3/ContentView.swift
@@ -6,27 +6,53 @@
 //
 
 import SwiftUI
-
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) { row in
+                HStack {
+                    ForEach(0 ..< self.columns) { column in
+                        self.content(row, columns)
+                    }
+                }
+            }
+        }
+    }
+    // adding init allow us making closures
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping(Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
 struct ContentView: View {
     // MARK: - Properties
     
     // MARK: - Body
     var body: some View {
-        Button("Hello World") {
-            print(type(of: self.body))
+        VStack{
+            GridStack(rows: 4, columns: 4) { row, col in
+                HStack {
+                    Image(systemName: "\(row * 4 + col).circle")
+                    Text("R\(row) C\(col)")
+                }
+            }
+            
+            GridStack(rows: 4 , columns: 4) { row, col in
+                Image(systemName: "clock")
+                Text("R\(row), C\(col)")
+            }
         }
-        .frame(width: 200, height: 200, alignment: .center)
-        .background(Color.red)
-        .padding()
-        .background(Color.yellow)
-        .padding()
-        .background(Color.blue)
     }
-}
-
-// MARK: - Preview
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    // MARK: - Preview
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
