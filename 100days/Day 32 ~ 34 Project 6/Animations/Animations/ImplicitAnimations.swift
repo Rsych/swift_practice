91e4efde761de5e79a8ad7b0c302f42869404b82
Ryan <tamielryan@gmail.com>
2021-10-06 20:34:15 +0900

    animations started

---
+++ 100days/Day 32 ~ 34 Project 6/Animations/Animations/ImplicitAnimations.swift
@@ -0,0 +1,20 @@
+//
+//  ImplicitAnimations.swift
+//  Animations
+//
+//  Created by Ryan J. W. Kim on 2021/10/06.
+//
+
+import SwiftUI
+
+struct ImplicitAnimations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImplicitAnimations_Previews: PreviewProvider {
+    static var previews: some View {
+        ImplicitAnimations()
+    }
+}
