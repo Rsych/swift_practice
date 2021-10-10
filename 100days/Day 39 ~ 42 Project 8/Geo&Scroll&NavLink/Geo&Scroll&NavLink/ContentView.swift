a8efe5be0eb205b72285c712a8fbbcd66baf0a73
Ryan <tamielryan@gmail.com>
2021-10-10 15:43:19 +0900

    some tutorials started

---
+++ 100days/Day 39 ~ 42 Project 8/Geo&Scroll&NavLink/Geo&Scroll&NavLink/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Geo&Scroll&NavLink
+//
+//  Created by Ryan J. W. Kim on 2021/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13")
+    }
+}
---
+++ 100days/Day 39 ~ 42 Project 8/Geo&Scroll&NavLink/Geo&Scroll&NavLink/ResizingGeometryReader.swift
@@ -0,0 +1,20 @@
+//
+//  ResizingGeometryReader.swift
+//  Geo&Scroll&NavLink
+//
+//  Created by Ryan J. W. Kim on 2021/10/10.
+//
+
+import SwiftUI
+
+struct ResizingGeometryReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResizingGeometryReader_Previews: PreviewProvider {
+    static var previews: some View {
+        ResizingGeometryReader()
+    }
+}
