eddbef50c09168271c0a4e8bbbbd6a6ec69edcc9
Ryan <tamielryan@gmail.com>
2021-10-11 22:04:34 +0900

    Geometry

--- 100days/Day 39 ~ 42 Project 8/Geo&Scroll&NavLink/Geo&Scroll&NavLink/ResizingGeometryReader.swift
+++ 100days/Day 39 ~ 42 Project 8/Geo&Scroll&NavLink/Geo&Scroll&NavLink/ResizingGeometryReader.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ResizingGeometryReader: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            GeometryReader { geo in
+            Image("example")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: geo.size.width)
+                
+        } //: Vstack
+        }
     }
 }
 
