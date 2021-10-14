78ed2f8e84cda348b446ab92779216de9336b530
Ryan <tamielryan@gmail.com>
2021-10-14 20:49:56 +0900

    Drawing hard

---
+++ 100days/Day 43 ~ 46 Project 9 - Drawing/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ryan J.W. Kim on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
