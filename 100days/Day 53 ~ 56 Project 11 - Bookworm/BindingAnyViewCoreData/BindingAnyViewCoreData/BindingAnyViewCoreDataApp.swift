f7d7e6ee3576f8ca1e6327278e7a210e3290d5ac
Ryan J. W. Kim <tamielryan@gmail.com>
2021-10-23 01:41:14 +0900

    Core Data practice

--- 100days/Day 53 ~ 56 Project 11 - Bookworm/BindingAnyViewCoreData/BindingAnyViewCoreData/BindingAnyViewCoreDataApp.swift
+++ 100days/Day 53 ~ 56 Project 11 - Bookworm/BindingAnyViewCoreData/BindingAnyViewCoreData/BindingAnyViewCoreDataApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct BindingAnyViewCoreDataApp: App {
+    let persistenceContainer = PersistenceController.shared // <- so need add this line
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceContainer.container.viewContext) // <- and here <-
+
         }
     }
 }
--- 100days/Day 53 ~ 56 Project 11 - Bookworm/BindingAnyViewCoreData/BindingAnyViewCoreData/ContentView.swift
+++ 100days/Day 53 ~ 56 Project 11 - Bookworm/BindingAnyViewCoreData/BindingAnyViewCoreData/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var whichTab = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $whichTab) {
+            CustomComponentWithBinding()
+                .tabItem {
+                    Label("Binding", systemImage: "1.circle")
+                }
+            
+            SizeClassAnyView()
+                .tabItem {
+                    Label("AnyView", systemImage: "2.circle")
+                }
+            CoreData()
+                .tabItem {
+                    Label("CoreData", systemImage: "3.circle")
+                }
+        }
     }
 }
 
