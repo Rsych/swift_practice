c2184ffb79422ac73082025c3ff3527b44709672
Ryan J. W. Kim <tamielryan@gmail.com>
2021-10-22 17:57:47 +0900

    CustomComponents with Binding

---
+++ 100days/Day 53 ~ 56 Project 11 - Bookworm/BindingAnyViewCoreData/BindingAnyViewCoreData/BindingAnyViewCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingAnyViewCoreDataApp.swift
+//  BindingAnyViewCoreData
+//
+//  Created by Ryan J. W. Kim on 2021/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BindingAnyViewCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
