75f56c30797b9e4f35c2ffc4722c223e1b665e29
Ryan J. W. Kim <tamielryan@gmail.com>
2021-10-24 22:14:50 +0900

    Deletefunction added Deletefunction and EditButton with moc.save()

--- 100days/Day 53 ~ 56 Project 11 - Bookworm/Bookworm/Bookworm/ContentView.swift
+++ 100days/Day 53 ~ 56 Project 11 - Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -23,32 +23,45 @@ struct ContentView: View {
             List {
                 ForEach(books, id: \.self) { book in
                     NavigationLink(destination: DetailView(book: book)) {
-                        EmojiRatingView(rating: book.rating)
-                            .font(.largeTitle)
-                    }//: NAV link
-                    VStack(alignment: .leading, spacing: nil) {
-                        Text(book.title ?? "Unknown title")
-                            .font(.headline)
-                        Text(book.author ?? "Unknown author")
-                            .foregroundColor(.secondary)
-                    }//: VStack
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                        }//: NAV link
+                        VStack(alignment: .leading, spacing: nil) {
+                            Text(book.title ?? "Unknown title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown author")
+                                .foregroundColor(.secondary)
+                        }//: VStack
+                    }//: HStack
                 }//: Loop
+                .onDelete(perform: deleteBooks) //: Delete
             }//: List
-                .navigationTitle("Bookworm")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            self.showingAddScreen.toggle()
-                        } label: {
-                            Image(systemName: "plus")
-                        } //: Button
-                    } //: NavBarTrailing
-                }//: Toolbar
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView().environment(\.managedObjectContext, self.moc)
-                } //: Sheet to load AddBookView
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        self.showingAddScreen.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    } //: Button
+                } //: NavBarTrailing
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                } //: Edit(Delete)
+            }//: Toolbar
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView().environment(\.managedObjectContext, self.moc)
+            } //: Sheet to load AddBookView
         }//: NavView
     }//: Body
+    func deleteBooks(at offSets: IndexSet) {
+        for offSet in offSets {
+            let book = books[offSet]
+            moc.delete(book)
+        }
+        try? moc.save()
+    } //: Delete function
 }//: ContentView
 
 struct ContentView_Previews: PreviewProvider {
