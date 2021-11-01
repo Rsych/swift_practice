43f53f7d82fefcfa327720ce19ed0f9bba640f02
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-01 21:35:07 +0900

    Query from Wiki

--- 100days/Day 68 ~ 73 Project 14 - Bucket List/BucketList/BucketList/EditView.swift
+++ 100days/Day 68 ~ 73 Project 14 - Bucket List/BucketList/BucketList/EditView.swift
@@ -13,6 +13,12 @@ struct EditView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var placemark: MKPointAnnotation
     
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
     // MARK: - Body
     var body: some View {
         NavigationView {
@@ -21,6 +27,21 @@ struct EditView: View {
                     TextField("Place name", text: $placemark.wrappedTitle)
                     TextField("Description", text: $placemark.wrappedsubtitle)
                 } //: Section
+                
+                Section {
+                    if loadingState == .loaded {
+                        List(pages, id:\.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    }
+                } header: {
+                    Text("Nearby")
+                } //: Section 2
+                
             } //: Form
             .navigationTitle("Edit title")
             .toolbar {
@@ -30,15 +51,45 @@ struct EditView: View {
                     } label: {
                         Text("Done")
                     }
-
+                    
+                }
+            } //: Toolbar
+            .onAppear {
+                fetchNearbyPlaces()
+            } // fetch soon as view appears
+        } //: NavView
+        
+    } //: bodyview
+        
+    func fetchNearbyPlaces() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                
+                if let items = try? decoder.decode(QueryWiki.self, from: data) {
+                    self.pages = Array(items.query.pages.values).sorted()
+                    self.loadingState = .loaded
+                    return
                 }
             }
-        } //: NavView
-    }
-}
+            loadingState = .failed
+        } .resume()
+    } //: fetch func
+} //: contentview
+
+
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
         EditView(placemark: MKPointAnnotation.example)
     }
 }
+
+
+
