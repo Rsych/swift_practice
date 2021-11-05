23a6b3ab8a70862e04c4bc40117538e85701d79b
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-05 22:16:01 +0900

    Generating and scan QR

--- 100days/Day 79 ~ 85 Project 16 - HotProspects/HotProspects/HotProspects/MeView.swift
+++ 100days/Day 79 ~ 85 Project 16 - HotProspects/HotProspects/HotProspects/MeView.swift
@@ -6,11 +6,55 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
+
 
 struct MeView: View {
+    // MARK: - Properties
+    @State private var name = ""
+    @State private var emailAddress = ""
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        NavigationView {
+            VStack {
+                
+                TextField("Name", text: $name, prompt: Text("Your name"))
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email address", text: $emailAddress, prompt: Text("your@email.com"))
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                Spacer()
+            } //: VStack
+            .navigationTitle("Your code")
+        } //: NAV View
+    } //: body
+    
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    } //: QRCode generator func
 }
 
 struct MeView_Previews: PreviewProvider {
--- 100days/Day 79 ~ 85 Project 16 - HotProspects/HotProspects/HotProspects/ProspectsView.swift
+++ 100days/Day 79 ~ 85 Project 16 - HotProspects/HotProspects/HotProspects/ProspectsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ProspectsView: View {
     // MARK: - Properties
@@ -14,6 +15,8 @@ struct ProspectsView: View {
     } //: Filtertype enum
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
+    
     let filter: FilterType
     
     var title: String {
@@ -27,27 +30,67 @@ struct ProspectsView: View {
         }
     } //: computed properties with switch
     
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter{ $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
     // MARK: - Body
     var body: some View {
         NavigationView {
-            Text("People: \(prospects.people.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            let prospect = Prospect()
-                            prospect.name = "Ryan"
-                            prospect.emailAddress = "aliefj@gmail.com"
-                            self.prospects.people.append(prospect)
-                        } label: {
-                            Label("Scan", systemImage: "qrcode.viewfinder")
-                        }
-
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
                     }
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        // for test
+//                        let prospect = Prospect()
+//                        prospect.name = "Ryan"
+//                        prospect.emailAddress = "aliefj@gmail.com"
+//                        self.prospects.people.append(prospect)
+                        isShowingScanner.toggle()
+                    } label: {
+                        Label("Scan", systemImage: "qrcode.viewfinder")
+                    }
+                    
+                }
+            } //: toolbar
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Ryan\nryan@ryan.com", completion: self.handleScan)
+            } //: qrscanner sheet
         } //: NAV View
     } //: body
-    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            self.prospects.people.append(person)
+        case .failure(let error):
+            print("Scan fail")
+        }
+    }
 }
 
 struct ProspectsView_Previews: PreviewProvider {
