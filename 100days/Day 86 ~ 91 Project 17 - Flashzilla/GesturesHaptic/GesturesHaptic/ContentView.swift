20b9380ce32db0ff6da7cd8d04799f12359c200b
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-07 16:43:48 +0900

    Gesture Drag

--- 100days/Day 86 ~ 91 Project 17 - Flashzilla/GesturesHaptic/GesturesHaptic/ContentView.swift
+++ 100days/Day 86 ~ 91 Project 17 - Flashzilla/GesturesHaptic/GesturesHaptic/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedTab) {
+            GesturesUse()
+                .tabItem {
+                    Label("GestureUse", systemImage: "1.circle")
+                } .tag(0)
+            Gesturespriorities()
+                .tabItem {
+                    Label("Gesture priority", systemImage: "2.circle")
+                } .tag(1)
+            GestureDrag()
+                .tabItem {
+                    Label("Drag", systemImage: "3.circle")
+                } .tag(2)
+        }
     }
 }
 
---
+++ 100days/Day 86 ~ 91 Project 17 - Flashzilla/GesturesHaptic/GesturesHaptic/GestureDrag.swift
@@ -0,0 +1,51 @@
+//
+//  GestureDrag.swift
+//  GesturesHaptic
+//
+//  Created by Ryan J. W. Kim on 2021/11/07.
+//
+
+import SwiftUI
+
+struct GestureDrag: View {
+    // MARK: - Properties
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+    
+    // MARK: - body
+    var body: some View {
+        let dragGesture = DragGesture()
+            .onChanged { value in offset =
+                value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    
+                isDragging = true
+                }
+            }
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+    }
+    
+}
+
+struct
+GestureDrag_Previews: PreviewProvider {
+    static var previews: some View {
+        GestureDrag()
+    }
+}
