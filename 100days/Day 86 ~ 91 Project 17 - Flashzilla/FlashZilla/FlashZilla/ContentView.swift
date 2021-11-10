cb5953efa2897c25092a1afb21627a429a70eaa3
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-10 15:33:46 +0900

    CardStack

--- 100days/Day 86 ~ 91 Project 17 - Flashzilla/FlashZilla/FlashZilla/ContentView.swift
+++ 100days/Day 86 ~ 91 Project 17 - Flashzilla/FlashZilla/FlashZilla/ContentView.swift
@@ -7,11 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        CardView(card: Card.example)
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
     }
 }
+
+struct ContentView: View {
+    // MARK: - Properties
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]).stacked(at: index, in: cards.count)
+                    } //: loop
+                } //: ZStack
+            } //: Vstack
+        } //: Zstack
+    } //: body
+} //: contentview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
