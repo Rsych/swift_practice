aff0c2e03a63c6888938fbc6348683c7a9a6ee29
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-12 23:17:35 +0900

    CustomAlignment

--- 100days/Day 92 ~ 94 Project 18 - LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/CustomAlignment.swift
+++ 100days/Day 92 ~ 94 Project 18 - LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/CustomAlignment.swift
@@ -7,12 +7,43 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+//    enum MidAccountAndName: AlignmentID { // usage same
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct CustomAlignment: View {
     // MARK: - Properties
     
     // MARK: - Body
     var body: some View {
-        Text("Hello, World!")
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@naolin")
+                    .alignmentGuide(.midAccountAndName) { d in
+                        d[VerticalAlignment.center]
+                    }
+                Image("example")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            } //: VStack
+            
+            VStack {
+                Text("Those two will be aligned no matter what")
+                Text("Even you add more text")
+                Text("Full name")
+                Text("Ryan J. W. Kim")
+                    .alignmentGuide(.midAccountAndName, computeValue: { d in
+                        d[VerticalAlignment.center]
+                    })
+                    .font(.largeTitle)
+            } //: VStack
+        } //: HStack
     }
 }
 
