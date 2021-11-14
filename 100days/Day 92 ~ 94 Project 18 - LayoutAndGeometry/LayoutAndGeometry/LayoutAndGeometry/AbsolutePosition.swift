d17b1efdec951563b54f280b4918337b0c85bdd7
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-14 19:46:24 +0900

    absolute position

---
+++ 100days/Day 92 ~ 94 Project 18 - LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/AbsolutePosition.swift
@@ -0,0 +1,20 @@
+//
+//  AbsolutePosition.swift
+//  LayoutAndGeometry
+//
+//  Created by Ryan J. W. Kim on 2021/11/14.
+//
+
+import SwiftUI
+
+struct AbsolutePosition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AbsolutePosition_Previews: PreviewProvider {
+    static var previews: some View {
+        AbsolutePosition()
+    }
+}
