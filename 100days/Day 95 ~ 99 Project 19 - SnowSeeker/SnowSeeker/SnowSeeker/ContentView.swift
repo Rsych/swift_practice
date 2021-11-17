ced0e800b27aad6befaa81ea56d17994eaa4905e
Ryan J. W. Kim <tamielryan@gmail.com>
2021-11-17 16:53:55 +0900

    NavView landscape

--- 100days/Day 95 ~ 99 Project 19 - SnowSeeker/SnowSeeker/SnowSeeker/ContentView.swift
+++ 100days/Day 95 ~ 99 Project 19 - SnowSeeker/SnowSeeker/SnowSeeker/ContentView.swift
@@ -35,12 +35,26 @@ struct ContentView: View {
                 } //: NavLink
             } //: List
             .navigationBarTitle("Resorts")
+            // secondary view
+            WelcomeView()
         } //: NavView
+        .phoneOnlyStackNavigationView()
     } //: body
 } //: contentview
+
+extension View {
+    func phoneOnlyStackNavigationView() -> some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return AnyView(self.navigationViewStyle(.stack))
+        } else {
+            return AnyView(self)
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
